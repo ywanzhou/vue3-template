f3c4fc611fc88c87b34bc6351c7760abaebb9b44
Zhou-Zhonghua <pacpc@qq.com>
2022-03-25 21:22:57 +0800

    fix(element-plus): 解决ElMessage和ElMessageBox样式引入失败的问题
    fix #1

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,8 @@ import router from './router'
 import store from './store'
 import './assets/css/index.css'
 import 'animate.css'
+// 单独引入 ElMessage 和 ElMessageBox 的样式
+import 'element-plus/theme-chalk/el-message.css'
+import 'element-plus/theme-chalk/el-message-box.css'
 
 createApp(App).use(router).use(store).mount('#app')
