5d295187b5d469727b53398a50e2b2858d20ce4c
Zhou-Zhonghua <pacpc@qq.com>
2022-03-25 21:23:41 +0800

    test(element-plus): 测试ElMessage和ElMessageBox样式是否导入成功

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,13 @@
 <template>
-  <el-button @click="get">请求</el-button>
-  <el-button
-    @click="cancelRequest('/api/common/weather/get15DaysWeatherByArea')"
-    >取消请求</el-button
-  >
-  <el-button @click="cancelAllRequest">取消全部请求</el-button>
+  <el-button type="primary" size="small" @click="open">测试</el-button>
+
   <router-view></router-view>
 </template>
 <script setup lang="ts">
-import request, { cancelRequest, cancelAllRequest } from './service'
-import { onMounted } from 'vue'
-
-interface Req {
-  apiKey: string
-  area?: string
-  areaID?: string
+import { ElMessage } from 'element-plus'
+const open = () => {
+  ElMessage({ message: '测试' })
 }
-interface Res {
-  area: string
-  areaCode: string
-  areaid: string
-  dayList: any[]
-}
-const get15DaysWeatherByArea = (data: Req) => {
-  return request<Req, Res>({
-    url: '/api/common/weather/get15DaysWeatherByArea',
-    method: 'GET',
-    data,
-    interceptors: {
-      requestInterceptors(res) {
-        return res
-      },
-      responseInterceptors(result) {
-        return result
-      },
-    },
-  })
-}
-const get = async () => {
-  const res = await get15DaysWeatherByArea({
-    apiKey: import.meta.env.VITE_APP_KEY,
-    area: '北京市',
-  })
-  console.log(res.result.dayList)
-}
-onMounted(() => {
-  get()
-})
 </script>
 
 <style></style>
