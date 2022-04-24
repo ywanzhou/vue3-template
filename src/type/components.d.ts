22dbdf1088b4b8ff951c63f142f76dd319e6520f
Zhou-Zhonghua <pacpc@qq.com>
2022-04-24 12:34:25 +0800

    style(d.ts): 调整d.ts的位置

--- src/type/components.d.ts
+++ src/type/components.d.ts
@@ -5,7 +5,6 @@
 declare module 'vue' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
   }
 }
 
