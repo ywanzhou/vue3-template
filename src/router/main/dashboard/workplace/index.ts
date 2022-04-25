1ac620c0f9af1f9dd2d4989ad7517ce8d5be8683
Zhou-Zhonghua <pacpc@qq.com>
2022-04-25 17:00:37 +0800

    feat(plop): remove page

--- src/router/main/dashboard/workplace/index.ts
+++
@@ -1,6 +0,0 @@
-const router = {
-  name: 'WorkPlace',
-  path: '/main/dashboard//dashboard/workplace',
-  component: () => import('@/views/main/dashboard/WorkPlace.vue'),
-}
-export default router
--- src/views/main/dashboard/WorkPlace.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <div>WorkPlace</div>
-</template>
-
-<style scoped></style>
