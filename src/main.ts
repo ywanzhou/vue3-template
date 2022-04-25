05270b5d70d40908206b99fff3f286ad6097a50f
Zhou-Zhonghua <pacpc@qq.com>
2022-04-26 00:03:21 +0800

    feat: 在注册路由之前将动态路由添加到路由中

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 // 引入 vue-router
 import router from './router'
-// 引入 vuex
+// 引入 pinia
+import { useUserStore } from './store'
 import store from './store'
 import './assets/css/index.css'
 import 'animate.css'
 // 单独引入 ElMessage 和 ElMessageBox 的样式
 import 'element-plus/theme-chalk/el-message.css'
 import 'element-plus/theme-chalk/el-message-box.css'
+const app = createApp(App).use(store)
 
-createApp(App).use(store).use(router).mount('#app')
+// 获取基础数据
+await useUserStore().getData()
+
+app.use(router).mount('#app')
