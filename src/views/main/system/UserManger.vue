ef9ce16d3a4fa9aedbf85f3545aa574a6e26bf88
Zhou-Zhonghua <pacpc@qq.com>
2022-04-26 22:28:15 +0800

    feat: 用户管理

--- src/views/main/system/UserManger.vue
+++ src/views/main/system/UserManger.vue
@@ -1,7 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useUserStore } from '/@/store'
+import YwzTable from '/@/components/ywz-table'
+import { ITableCol } from '/@/components/ywz-table/types'
+const store = useUserStore()
+const userList = computed(() => store.userList)
+console.log(userList.value)
+const columns: ITableCol[] = [
+  { label: '用户名', prop: 'name' },
+  { label: '创建时间', prop: 'updateTime' },
+  { label: '角色', prop: 'role', slotName: 'role' },
+]
+</script>
 
 <template>
-  <div>UserManger</div>
+  <YwzTable :table-data="userList" :columns-list="columns">
+    <template #role="scoped">
+      <el-tag
+        :type="
+          scoped.row.role.name === 'superAdmin'
+            ? 'success'
+            : scoped.row.role.name === 'admin'
+            ? 'warning'
+            : 'danger'
+        "
+        size="small"
+        effect="light"
+        >{{ scoped.row.role.name }}</el-tag
+      >
+    </template>
+  </YwzTable>
 </template>
 
 <style scoped></style>
