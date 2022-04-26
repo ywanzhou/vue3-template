ba4f47d47888917ba1eff0a8e4639eefc03aa798
Zhou-Zhonghua <pacpc@qq.com>
2022-04-26 22:36:38 +0800

    feat: 权限管理

--- src/views/main/system/PermissionManger.vue
+++ src/views/main/system/PermissionManger.vue
@@ -1,7 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useUserStore } from '/@/store'
+import YwzTable from '/@/components/ywz-table'
+import { ITableCol } from '/@/components/ywz-table/types'
+const store = useUserStore()
+const permissionList = computed(() => store.permissionList)
+const columns: ITableCol[] = [
+  { label: '标题', prop: 'name' },
+  { label: '类型', prop: 'type', slotName: 'type' },
+  { label: '路径', prop: 'path' },
+]
+</script>
 
 <template>
-  <div>PermissionManger</div>
+  <YwzTable :table-data="permissionList" :columns-list="columns">
+    <template #type="scoped">
+      <el-tag type="success">
+        {{ scoped.row.type === 0 ? '跟路由' : '二级路由' }}
+      </el-tag>
+    </template>
+  </YwzTable>
 </template>
 
 <style scoped></style>
